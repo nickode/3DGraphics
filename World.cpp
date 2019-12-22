0b81174f4a11f190be661ae507e1083a2ba11cc1
nickode <nicolasnsamaha@hotmail.com>
2019-12-22 03:53:08 -0500

    gitignore

---
+++ World.cpp
@@ -0,0 +1,11 @@
+#include <World.h>
+
+World::World()
+{
+	model = new glm::mat4(1.0f);
+}
+
+World::~World()
+{
+
+}
---
+++ World.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <openglstarterpack.h>
+
+class World
+{
+public:
+	glm::mat4* model;
+	World();
+	~World();
+
+
+};
