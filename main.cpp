61619c54cfb008694b87ed21371fd7da0e04d0c7
nickode <nicolasnsamaha@hotmail.com>
2019-12-30 14:57:07 -0500

    loading half a cylinder barely...

--- main.cpp
+++ main.cpp
@@ -36,7 +36,10 @@ int main()
 	std::string warn;
 	std::string err;
 
-	bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename);
+	bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename,NULL,true);
+
+	std::cout << attrib.vertices.size() << std::endl<< attrib.normals.size() << std::endl << attrib.texcoords.size() << std::endl << attrib.vertex_weights.size();
+
 
 	window = initOpenGL(800,600);
 	glfwSetCursorPosCallback(window, mouse_callback_fpv);
@@ -44,26 +47,39 @@ int main()
 	vao = new VertexArray();
 	vbo = new VertexBuffer();
 	Shader shader("vertex.shader","fragment.shader");
+
+	std::vector<glm::vec3> vertices;
+	for (int i = 0; i < attrib.vertices.size(); i = i+3)
+	{
+		glm::vec3 vertex; 
+		if (i + 1 > attrib.vertices.size())
+			break;
+		vertex.x = attrib.vertices[i];
+		vertex.y = attrib.vertices[i+1];
+		vertex.z = attrib.vertices[i+2];
+		vertices.push_back(vertex);
+	}
 	
 	//Vertex buffers and arrays
 	vao->bind();
 	vbo->bind(); 
-	glBufferData(GL_ARRAY_BUFFER, attrib.vertices.size() * sizeof(float), &attrib.vertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
 	
-	vao->enable(0, 3, GL_FLOAT, GL_FALSE, 11*sizeof(float), (void*)0);
-	vao->enable(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(sizeof(float)*3));
-	vao->enable(2, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(sizeof(float) * 3));
-	vao->enable(3, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(float), (void*)(sizeof(float)*2));
+	vao->enable(0, 3, GL_FLOAT, GL_FALSE, 0*sizeof(float), (void*)0);
+	//vao->enable(1, 3, GL_FLOAT, GL_FALSE, 0 * sizeof(float), (void*)3);
+	
+	
+
 	vao->unbind();
 
 	shader.use();
-	glm::vec3* model = new glm::vec3(1.0f);
+	glm::mat4* model = new glm::mat4(1.0f);
 	glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "model"), 1, GL_FALSE, glm::value_ptr(*model));
 	glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "projection"), 1, GL_FALSE, glm::value_ptr(cam.projection));
 
 	GLint iView = glGetUniformLocation(shader.getProgram(), "view");
 	
-	std::thread fps(countFps);
+	//std::thread fps(countFps);
 	//std::thread input(processInput);
 
 
@@ -71,8 +87,11 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{		
-		fpsFlag++;
-		while (!fpsFlag);
+		//fpsFlag++;
+		//while (!fpsFlag);
+		currentTime = glfwGetTime();
+		deltaTime = float(currentTime - lastTime);
+		lastTime = currentTime;
 		processInput();
 		*cam.view = glm::lookAt(*cam.pos, *cam.pos + *cam.front, *cam.up);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -82,7 +101,7 @@ int main()
 		//glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "projection"), 1, GL_FALSE, glm::value_ptr(cam.projection));
 
 		vao->bind();
-		glDrawArrays(GL_TRIANGLES, 0, 3 * shapes.size());
+		glDrawArrays(GL_TRIANGLES, 0, 64);
 		vao->unbind();
 
 		glfwSwapBuffers(window);
@@ -91,7 +110,7 @@ int main()
 	}
 
 	stopFps++;
-	fps.join();
+	//fps.join();
 	//stopInput++;
 	//input.join();
 
