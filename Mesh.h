65178c9dc640be09d0fa178a0e8e925ef1e50782
nickode <nicolasnsamaha@hotmail.com>
2020-01-05 12:28:11 -0500

    assimp and model clas

--- Mesh.h
+++ Mesh.h
@@ -4,17 +4,36 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <assimp/material.h>
+#include <assimp/StringUtils.h>
+#include <assimp/texture.h>
+#include <assimp/types.h>
+#include <assimp/mesh.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
 
+struct Texture {
+	unsigned int id;
+	std::string type;
+	aiString path;
+};
 
 class Mesh
 {
 public:
-
-
-	std::vector <unsigned int> v_indices;
-
-	unsigned int nb_vertices = 0;
-	void loadFromObjFile(const char* filename);
-	void tinyLoader(const char* filename);
+	std::vector <Vertex> vertices;
+	std::vector <unsigned int> indices;
+	std::vector <Texture> textures;
+	Mesh(std::vector<Vertex> v, std::vector<unsigned int> i, std::vector<Texture> t);
+	void Draw();
+private:
+	unsigned int VAO, VBO, EBO;
+	void setupMesh();
 };
